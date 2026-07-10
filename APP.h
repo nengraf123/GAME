836cab620139bbabc9af860d9f38c4fee157fa02
nengraf123 <nengraf123@gmail.com>
2026-07-10 21:54:45 +0300

    update 2026-07-10 21:54

--- APP.h
+++ APP.h
@@ -4,8 +4,8 @@
 #include <raymath.h>
 #include "player/Player.h"
 
-const int VIRTUAL_WIDTH = 1920;
-const int VIRTUAL_HEIGHT = 1080;
+const int MONITOR_WIDTH = 1920;
+const int MONITOR_HEIGHT = 1080;
 
 class APP {
 public:
