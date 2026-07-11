b724ae51f57991b349b5fdd712bb4cef4ae60e36
nengraf123 <nengraf123@gmail.com>
2026-07-11 06:42:05 +0300

    update 2026-07-11 06:42

--- While.cc
+++ While.cc
@@ -6,8 +6,6 @@
 using namespace std;
 
 
-bool init = false;
-
 int X_ = 200, Y_ = 200, W_ = 200, H_ = 200;
 
 void APP::While() {
@@ -19,26 +17,19 @@ void APP::While() {
     float dt = GetFrameTime();
     BeginDrawing();
       ClearBackground(WHITE);
-
-
       camera.target = Vector2Add(player.getPosition(), Vector2Scale(player.getSize(), 0.5f));
       camera.offset = Vector2{ (float)GetScreenWidth() / 2.0f, (float)GetScreenHeight() / 2.0f };
       BeginMode2D(camera);
-          
-          DrawRectangle(X_, Y_, W_, H_, BLUE); 
-          if (X_ == 1520) X_=X_-10;
-          if (X_ != 1520) X_=X_+10;
-
-          player.draw();
-
+        // обьекты которые приикреплены к миру (кроме игрока, камера двигается за ним)
+        DrawRectangle(X_, Y_, W_, H_, BLUE); 
+        if (X_ == 1520) X_=X_-10;
+        if (X_ != 1520) X_=X_+10;
+        player.logica(dt);
+        player.draw();
       EndMode2D(); // Конец режима камеры
-
-      player.logica(dt);
-      // player.draw();
-
-      panel.logica(dt);
+      // обьекты которые прикреплены к монитору (например панель)
       panel.draw();
-
+      panel.logica(dt);
     EndDrawing();
   }
   player.unload();
